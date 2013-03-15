e8c578eda773c379bc5ade74f3b14d7ec0146659
blennon <blennon86@gmail.com>
2013-03-15 14:27:53 -0700

    updated cerebellum neuron groups to use synapse object and simplified neuron model equations.  began work on gogli cells, not working yet.

--- neuron_models/cerebellum.py
+++ neuron_models/cerebellum.py
@@ -25,7 +25,6 @@ class GranuleCellGroup(NeuronGroup):
     tau_inh1 = 7. * msecond      # Inhbitory time constant 1
     tau_inh2 = 59. * msecond     # Inhbitory time constant 2
     tau_ahp = 5. * msecond       # AHP time constant
-    tau_dt = defaultclock.dt     # dt time constant so dg_glut, dg_gaba decay completely in one step
     
     eqns = Equations('''
     # Membrane equation
@@ -35,14 +34,12 @@ class GranuleCellGroup(NeuronGroup):
     dgahp/dt = -gahp/tau_ahp : nS
     
     # Glutamate
-    dg_glut/dt = -g_glut/tau_dt : 1
-    dg_ampa/dt = -g_ampa/tau_ampa + g_ampa_*g_glut/tau_dt : nS
-    dg_nmda/dt = -g_nmda/tau_nmda + g_nmda_*g_glut/tau_dt: nS
+    dg_ampa/dt = -g_ampa/tau_ampa : nS
+    dg_nmda/dt = -g_nmda/tau_nmda : nS
     
     # GABA
-    dg_gaba/dt = -g_gaba/tau_dt : 1
-    dg_inh1/dt = -g_inh1/tau_inh1 + g_inh_*g_gaba/tau_dt: nS
-    dg_inh2/dt = -g_inh2/tau_inh2 + g_inh_*g_gaba/tau_dt: nS
+    dg_inh1/dt = -g_inh1/tau_inh1 : nS
+    dg_inh2/dt = -g_inh2/tau_inh2 : nS
     g_inh = .43 * g_inh1 + .57 * g_inh2 : nS
     ''')
     
@@ -53,24 +50,45 @@ class GranuleCellGroup(NeuronGroup):
         
 class GolgiCellGroup(NeuronGroup):
     '''
-    Group of N Golgi cells
+    Group of Golgi cells
     '''
-    Vth = -35 * mvolt
-    Cm = 3.1 * pfarad
-    gl = 0.42 * nsiemens
-    El = -58 * mvolt
-    g_ampa = 0.18 * nsiemens
-    g_nmda = 0.025 * nsiemens
-    Eex = 0. * mvolt
-    g_inh = 0.028 * nsiemens
-    Einh = -82. * mvolt
-    gahp = 1. * nsiemens
-    Eahp = -82. * mvolt
-    tau_ahp = 5. * msecond
+    Vth = -52. * mvolt            # Firing threshold, Volts
+    Cm = 28. * pfarad            # Membrane capacitance
+
+    El = -55. * mvolt             # leak reversal potential
+    Eex = 0. * mvolt             # Excitatory reversal potential
+    Einh = -82. * mvolt          # Inhibitory reversal potential
+    Eahp = -72.7 * mvolt          # After hyperpolarization reversal potential
+
+    gl = 2.3 * nsiemens         # maximum leak conductance
+    g_ampa_ = 45.5 * nsiemens    # maximum ampa conductance
+    g_nmda_ = 30. * nsiemens   # maximum nmda conductance
+    gahp_ = 20. * nsiemens        # maximum after hyperpolarization conductance
+    
+    tau_ampa = 1.5 * msecond     # AMPA time constant
+    tau_nmda1 = 31. * msecond     # NMDA time constant 1
+    tau_nmda2 = 170. * msecond     # NMDA time constant 2    
+    tau_ahp = 5. * msecond       # AHP time constant
+    
+    eqns = Equations('''
+    # Membrane equation
+    dV/dt = 1/Cm*(-gl*(V-El)-g_ampa*(V-Eex)-g_nmda*(V-Eex)-g_inh*(V-Einh)-gahp*(V-Eahp)) : mV
+    
+    # After hyperpolarization
+    dgahp/dt = -gahp/tau_ahp : nS
+    
+    # Glutamate
+    dg_ampa/dt = -g_ampa/tau_ampa : nS
+    dg_nmda1/dt = -g_nmda1/tau_nmda1 : nS
+    dg_nmda2/dt = -g_nmda1/tau_nmda2 : nS
+    g_nmda = .33 * g_nmda1 + .67 * g_nmda2 : nS
+
+    ''')
     
     def __init__(self, N):
         super(GranuleCellGroup, self).__init__(N, model=GolgiCellGroup.eqns,threshold=GolgiCellGroup.Vth,
-                                               reset='V=GogliCellGroup.El;gahp=GolgiCellGroup.gahp_')
+                                               reset='V=GolgiCellGroup.El;gahp=GolgiCellGroup.gahp_')
         
 if __name__ == "__main__":
     GR = GranuleCellGroup(1)
+    GO = GolgiCellGroup(1)
