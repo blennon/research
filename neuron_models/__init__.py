b6f2f3b18a0a5c7a3371b65decb2a34cf9f788d3
blennon <blennon86@gmail.com>
2013-04-21 11:22:42 -0700

    amended init to include abstract neuron group and yamazaki neuron

--- neuron_models/__init__.py
+++ neuron_models/__init__.py
@@ -1,4 +1,7 @@
+from abstract_neuron_group import *
 from granular_layer import *
+from purkinje_layer import *
+from yamazaki_neuron import *
 from plotting_util import *
 from similarity import *
 from stimuli import *
