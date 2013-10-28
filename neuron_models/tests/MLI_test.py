ce2757685c9d4a148ff059d284e857e114aefe10
blennon <blennon86@gmail.com>
2013-10-28 11:16:01 -0700

    removed basket_test.  changed dt in MLI_test

--- neuron_models/tests/MLI_test.py
+++ neuron_models/tests/MLI_test.py
@@ -7,7 +7,7 @@ import unittest
 from pylab import *
 from brian import *
 from neuron_models import *
-defaultclock.dt = 1.*ms
+defaultclock.dt = .25*ms
 
 class MLITest(unittest.TestCase):      
 
@@ -18,13 +18,13 @@ class MLITest(unittest.TestCase):
         self.assertEquals(params['Vth'], -53. * mV)
         self.assertEquals(params['Cm'], 14.6 * pF)
         self.assertEquals(params['gl'], 1.6 * nS)
-        self.assertEquals(params['El'], -65. * mV)
+        self.assertEquals(params['El'], -68. * mV)
         self.assertEquals(params['g_ampa_'], 1.3 * nS)
         self.assertEquals(params['Eex'], 0. * mV)
         self.assertEquals(params['tau_ampa'], .8 * ms)
         self.assertEquals(params['g_ahp_'], 50. * nS)
         self.assertEquals(params['Eahp'], -82. * mV)
-        self.assertEquals(params['tau_ahp'], 5 * ms)
+        self.assertEquals(params['tau_ahp'], 2.5 * ms)
         
 
 if __name__ == "__main__":
