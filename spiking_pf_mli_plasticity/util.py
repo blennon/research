0b52682e80b10160da14122d7ec58f2c43d8438b
bill <blennon86@gmail.com>
2014-12-01 10:29:00 -0800

    added utility functions for saving experiment data. added cells to ipynbs to save data

--- spiking_pf_mli_plasticity/util.py
+++ spiking_pf_mli_plasticity/util.py
@@ -1,5 +1,38 @@
 __author__ = 'bill'
 from brian import *
+import os, errno
+import cPickle
+import datetime
+
+def mkdir_p(path):
+    '''
+    recursively create directories.
+    
+    credit: tzot, Craig Ringer
+    from: http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+def pickle_results(objects_dict, exper_dir):
+    '''
+    objects_dict: a dictionary of (object string, object) where the object contains data
+    exper_dir: the top-level directory to save results for this experiment
+    '''
+    today = datetime.datetime.today()
+    out_dir = exper_dir+today.strftime("%m-%d-%Y_%H:%M")+'/'
+    mkdir_p(out_dir)
+    for s,obj in objects_dict.iteritems():
+        if '_S' in s:
+            with open(out_dir+'%s.pkl'%s,'w') as f:
+                cPickle.dump(obj.spiketimes, f)
+        else:
+            with open(out_dir+'%s.pkl'%s,'w') as f:
+                cPickle.dump(obj.getvalues(), f)
 
 def pf_rates(t):
     '''
